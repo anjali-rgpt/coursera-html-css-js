59c73a3fffa10aa9adf3f8d0a5f8cc94fa24ca19
anjali-rgpt <anjur99@gmail.com>
2020-11-06 01:52:09 +0530

    Updated

--- module4files/SpeakHello.js
+++ module4files/SpeakHello.js
@@ -14,6 +14,7 @@ var speakWord = "Hello";
 
 var helloSpeaker={};
 
+
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
