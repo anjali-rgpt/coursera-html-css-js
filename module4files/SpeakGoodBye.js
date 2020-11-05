997e9f776a4fdc777c8cf91a69b0e4e3569cad7e
anjali-rgpt <anjur99@gmail.com>
2020-11-06 01:42:17 +0530

    JS IIFE

--- module4files/SpeakGoodBye.js
+++ module4files/SpeakGoodBye.js
@@ -12,13 +12,14 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-(function(name){
+(function(window){
 var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-var byeSpeaker = function(name) {
+var byeSpeaker={};
+byeSpeaker.speak = function(name) {
   console.log(speakWord + " " + name);
 }
 
